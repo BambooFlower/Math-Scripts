5fd7119b2d9bac0b61b2a1af9da3295f48e8fe02
Pietro Petitti <19675218+BambooFlower@users.noreply.github.com>
2020-04-06 17:45:54 +0200

    Add files via upload

---
+++ Code/GaussSeidel.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+ITERATION_LIMIT = 1000
+
+# initialize the matrix
+A = np.array([[10., -1., 2., 0.],
+              [-1., 11., -1., 3.],
+              [2., -1., 10., -1.],
+              [0., 3., -1., 8.]])
+# initialize the RHS vector
+b = np.array([6., 25., -11., 15.])
+
+print("The system of equations is:")
+for i in range(A.shape[0]):
+    row = ["{0:3g}*x{1}".format(A[i, j], j + 1) for j in range(A.shape[1])]
+    print("[{0}] = [{1:3g}]".format(" + ".join(row), b[i]))
+
+x = np.zeros_like(b)
+for it_count in range(1, ITERATION_LIMIT):
+    x_new = np.zeros_like(x)
+    print("Iteration {0}: {1}".format(it_count, x))
+    for i in range(A.shape[0]):
+        s1 = np.dot(A[i, :i], x_new[:i])
+        s2 = np.dot(A[i, i + 1:], x[i + 1:])
+        x_new[i] = (b[i] - s1 - s2) / A[i, i]
+    if np.allclose(x, x_new, rtol=1e-8):
+        break
+    x = x_new
+
+print("The solution is: {0}".format(x))
+error = np.dot(A, x) - b
+print("The error in the approximation is: {0}".format(error))
