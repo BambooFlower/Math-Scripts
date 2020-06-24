7c7a55fc056957f63f2905d537411318ff56b5ed
Pietro Petitti <19675218+BambooFlower@users.noreply.github.com>
2020-06-24 20:50:53 +0100

    Add files via upload

--- Code/AudioVisualizer/AudioRender.py
+++ Code/AudioVisualizer/AudioRender.py
@@ -1,52 +1,58 @@
 # Render the audio of the wav file
 # The rendered audio is then visualized in AudioVisualizer.py
 
-
-from scipy.io import wavfile
 import numpy as np
-from skimage import util
 from scipy.fftpack import fft
-import matplotlib.pyplot as plt
+from pydub import AudioSegment
 
 class Audio_fft():
-    def __init__(self, filename, stereo=True, M=2048):
-        self.rate, self.audio = wavfile.read(filename)
+    def __init__(self, filename,M=2048,group_num=16):
+        self.song = AudioSegment.from_file(filename)
+        
+        self.rate = self.song.frame_rate
         
         self.M = M
-        # Average out the left and right channels
-        if(stereo):
-            self.audio = np.mean(self.audio, axis=1)
-            
+        self.max_amp_raw = self.song.max_possible_amplitude
+        self.max_amp = 2**(np.log2(self.max_amp_raw)*1.10)
         
-    def get_fft(self,slice_num, group_num=16, get_freq_space=False,grouped=True,localAvg=False):
-        song_slice = self.audio[slice_num[0]:slice_num[1]]
+        self.num_groups = group_num
+        self.groups = self.gen_groups(group_num)        
+        
+    def gen_groups(self,num_groups):
+        step_size = 1/num_groups
+        out = []
+        for i in range(num_groups):
+            out.append(15.877*np.exp(i*step_size*7.1274))
+        return out
+                
+    def get_fft(self,slice_num, group_num=16, get_freq_space=False,
+                grouped=True,localAvg=False):
+        
+        song_slice = self.song.get_sample_slice(slice_num[0],slice_num[1]).get_array_of_samples()
         spectrum = fft(song_slice)
-        spectrum = np.abs(spectrum)[:self.M//2]  # Remove the second half
-        
+
+        # Remove the second half, since the FFT of real frequencies is symmetric
+        spectrum = np.abs(spectrum)[:self.M//2]  
+
         self.freq_space = (self.rate / self.M/2)
         
-        
         # Return not grouped fft
-        if(not grouped):
+        if not grouped:
             return spectrum
-        
-        # Frequency spectrum
-        groups = [20,25,31.5,40,50,63,80,100,125,160,200,250,315,400,
-                500,630,800,1000,1200,1600,2000,2500,3000,4000,5000,
-                6300,8000,10000,12000,16000,20000]
-        
+
         # Split array
         pos = 0
-        separated_arrs = [0]*len(groups)
+        separated_arrs = [0]*self.num_groups
         
-        for i in range(round(self.M/2)):
-            if(groups[pos] <= (i+1)*self.freq_space):
+        for i in range(self.M//2):
+            if(self.groups[pos] <= (i+1)*self.freq_space):
                 pos += 1
             separated_arrs[pos] += spectrum[i]
-            
+
+           
         if not localAvg:
             separated_arrs = np.nan_to_num(np.array(separated_arrs))
-            return separated_arrs / (2**25)
+            return separated_arrs / (self.max_amp)
              
         
         # Workout averages
@@ -59,9 +65,12 @@ class Audio_fft():
         means /= (means).max()
         return means
     
+
     def get_freq_array(self):
         freq_space = (self.rate / self.M/2)
         return np.linspace(0,self.M*(freq_space+0.1),self.M/2)
     
+    
     def get_wave(self,slice_num):
-        return self.audio[slice_num[0]:slice_num[1]]
+        return self.song.get_sample_slice(slice_num[0],slice_num[1]).get_array_of_samples()
+    
--- Code/AudioVisualizer/AudioVisualizer.py
+++ Code/AudioVisualizer/AudioVisualizer.py
@@ -4,31 +4,42 @@
 
 import pygame
 from pygame import mixer
+import matplotlib.pyplot as plt
 import AudioRender as Audio
 import time
 import numpy as np
-import sys
  
 class Renderer():
     def __init__(self,resolution=(800,720),fps=60):
-        M = 2048 # Slice size
+        M = 1024  # Slice size
 
         # Load the song
-        songName = "backstage.wav"
-        song = Audio.Audio_fft(songName,True, M=M)
+        songName = "backstage.mp3"
         
-        # Initialize the visualizer
-        pygame.init()
+        # Number of FFT bar groups
+        self.num_groups = 31
+
+        song = Audio.Audio_fft(songName, M=M,group_num=self.num_groups)
+        
+        self.max_amp = song.max_amp
+        self.max_amp_raw = song.max_amp_raw
+
+        # Initalise visualiser
+        pygame.init()        
+        # Initialize mixer
+        pygame.mixer.quit()
+        pygame.mixer.init(frequency=song.rate)
         pygame.mixer.music.load(songName)
         pygame.mixer.music.play(0)
 
         clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode(resolution)
-        self.screen.fill(pygame.Color('white'))
          
         # Close the program if the user clicks the close button
         done = False
         while not done:
+            self.screen.fill(pygame.Color('white'))
+            
             song_time = pygame.mixer.music.get_pos()
 
             for event in pygame.event.get():   
@@ -38,15 +49,17 @@ class Renderer():
                     mixer.unpause()
                 if event.type == pygame.QUIT:  
                     done = True   
-
+              
             seconds = song_time/1000
+            
             try:
                 scale = int(np.floor(song.rate/M*seconds))
                 slice_num = [M*(scale),M*(scale+1)]
-
+    
                 self.draw_fourier(song.get_fft(slice_num,grouped=True,localAvg=False))
                 self.draw_raw(song.get_wave(slice_num))
-            except:
+
+            except ValueError:
                 break
                 
                 
@@ -60,32 +73,89 @@ class Renderer():
 
             # Update the screen 
             pygame.display.flip()
-            clock.tick(60)
+            clock.tick(fps)
+
+        pygame.quit()
 
     def draw_fourier(self,data):
         # Draw the 29 frequency bands
 
+        left_top = (10,10)
+        width_height = (784,300)
+
+        graph_col = (50,50,50)
         
         # Draw the box
-        pygame.draw.rect(self.screen,(255,255,255),(10,10,780,300))
-        pygame.draw.rect(self.screen,(0,0,0),(10,10,780,300),2)
+        pygame.draw.rect(self.screen,(255,255,255),(left_top,width_height))
+        pygame.draw.rect(self.screen,graph_col,(left_top,width_height),2)
         
-        for i in range(0,29):
+        for i in range(1,self.num_groups-1):
             if(data[i] >= 0):
-                pygame.draw.rect(self.screen,(0,0,0),(9+10+26.5*i,310-2,20,-0.6*295*data[i]))
+                bar_h = data[i]
+                bar_w = int((width_height[0])/(self.num_groups-2))
+                box_x = int(left_top[0] + bar_w*i - bar_w)
+                if bar_h > width_height[1] - left_top[0]//2:
+                    bar_h = width_height[1] - left_top[0]//2
+
+                pygame.draw.rect(self.screen,graph_col,(box_x,
+                                                      308,   
+                                                      bar_w,
+                                                      int(-bar_h))
+                                 )
     
     def draw_raw(self, data):
         # Draw the raw music frequency
 
-        pygame.draw.rect(self.screen,(255,255,255),(144,370,512,300))
-        pygame.draw.rect(self.screen,(0,0,0),(144,370,512,300),2)
+        left_top = (144,370)
+        width_height = (512,300)
+
+        pygame.draw.rect(self.screen,(255,255,255),(left_top,width_height))
+        pygame.draw.rect(self.screen,(0,0,0),(left_top,width_height),2)
         
         dat = np.array(data)
         
         avg = np.mean(dat.reshape(-1, 4), axis=1)
-        avg /= 2**15
+        avg /= (self.max_amp_raw)
         
-        for i in range(len(avg)):
-            pygame.draw.rect(self.screen,(0,0,0),(144+i,525,2.75,-0.8*150*avg[i]))
+        num_els = len(avg)
+        rec_w = int(width_height[0]/num_els)
+        
 
-app = Renderer(fps=30)
+        # Circular Instogram using matplotlib
+        # N = 80
+        # bottom = 8
+        # max_height = 4
+        # theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False)
+        # radii = max_height*np.random.rand(N)
+        # width = (2*np.pi) / N
+
+        # ax = plt.subplot(111, polar=True)
+        # bars = ax.bar(theta, radii, width=width, bottom=bottom)
+        # plt.show()
+
+
+
+
+
+
+        for i in range(num_els):
+
+            # Failed attemps in pygame 
+            # Center of the box is given (405,520)
+            # pygame.draw.circle(self.screen, (0,0,255), (400, 520), 15, 1)
+            # pygame.draw.arc(self.screen, (0,0,255), (405, 520, 100*rec_w, 100), 0, 6.283, 1)
+            # pygame.draw.arc(self.screen, (255,0,0), (int(left_top[0]+i*rec_w),
+            #     int(left_top[1]+width_height[1]//2), rec_w, int(-(width_height[1]//2)*avg[i])),
+            #         0, 6.283, 1)    # 2pi is 6.283
+
+
+            pygame.draw.rect(self.screen,(0,0,0),(int(left_top[0]+i*rec_w),
+                                                  int(left_top[1]+width_height[1]//2),
+                                                  rec_w,
+                                    int(-(width_height[1]//2)*avg[i])), 1
+                             )
+
+
+            
+
+app = Renderer(fps=24)
