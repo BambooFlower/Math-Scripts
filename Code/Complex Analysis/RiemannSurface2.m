cdd0f6246ef5cb824bff71dfd8fdd1da114ab0ba
BambooFlower <19675218+BambooFlower@users.noreply.github.com>
2020-12-20 19:41:14 +0000

    Update

--- Code/Complex Analysis/RiemannSurface2.m
+++ Code/Complex Analysis/RiemannSurface2.m
@@ -1,21 +1,21 @@
-% Rieman surface for f(z) = sqrt(z)
+% Rieman surface for f(z) = log(z)
 
 close all
-clear
+clear 
 
 % create a matrix of complex inputs
 r = transpose(0:1:15);    
 
-theta = pi*(-2:0.05:2);
+theta = pi*(-3:0.05:3);
 z = r*exp(1i*theta);
 
 % calculate the complex outputs
-w = sqrt(r)*exp(1i*theta/2);
+w = log(r) + (1i*theta/2);
 
-figure('Name','Complex Graph','units','normalized','outerposition',[ 0.08 0.1 0.8 0.55]);
+figure('Name', 'Complex Graph','units','normalized','outerposition',[ 0.08 0.1 0.8 0.55]);
 subplot(121)
 
-% visualize the complex function using surf
+% visualize the real part of the complex function using surf
 surf(real(z),imag(z),real(w),imag(w))    
 xlabel('Real(z)')
 ylabel('Imag(z)')
@@ -27,7 +27,7 @@ colormap jet
 axis tight
 
 
-% visualize the complex function using surf
+% visualize the imaginary part of the complex function using surf
 subplot(122)
 surf(real(z),imag(z),imag(w),real(w)) 
 
